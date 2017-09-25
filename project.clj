6ca3ea5de6abb84698e0d0f8bcb36f6e518db576
Upadhyay <prayag.upd@gmail.com>
2017-09-25 00:23:38 -0700

    update clojure version, but none working might be because of proxy

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.2.374"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.443"]
+                 ]
+  :user {:repository [["clojars" "http://clojars.org/repo/"]
+               ["sonatype" "https://oss.sonatype.org/content/repositories/releases/"]
+               ["mvncentral" "http://central.maven.org/maven2/"]]}
+
   :main ^:skip-aot reactive-shipping.core
   )
